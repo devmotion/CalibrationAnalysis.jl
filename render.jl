c10f277946acfbf9715d97f29485015261428b16
David Widmann <david.widmann@it.uu.se>
2022-09-25 00:56:18 +0200

    Address Fredrik's comments

--- render.jl
+++ render.jl
@@ -3,5 +3,12 @@ using CondaPkg
 # Render document
 CondaPkg.withenv() do
     python = CondaPkg.which("python")
-    run(addenv(`quarto render paper.qmd --to jss-latex`, "QUARTO_PYTHON" => python))
+    run(
+        addenv(
+            `quarto render paper.qmd --to jss-latex`,
+            "QUARTO_PYTHON" => python,
+            # Avoid confirmation dialog and accept license of Palmer penguins data
+            "DATADEPS_ALWAYS_ACCEPT" => "true",
+        ),
+    )
 end
